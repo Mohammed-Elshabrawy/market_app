31fc2901252c60c8be3dcd11e14d761549ce86a4
Mohammed-Elshabrawy <m.e18520029@gmail.com>
2025-06-19 03:25:48 +0300

    finish store_screen.dart

--- lib/modules/store/ui/store_screen.dart
+++ lib/modules/store/ui/store_screen.dart
@@ -1,11 +1,36 @@
 import 'package:flutter/material.dart';
+
+import '../../../shared/components/custom_search_filed.dart';
+import '../../../shared/components/product_list.dart';
+
 class StoreScreen extends StatelessWidget {
   const StoreScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Hello Store'),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView(
+        children: [
+          Center(
+            child: Text(
+              "Welcome to Market App",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                height: 1,
+              ),
+            ),
+          ),
+          SizedBox(height: 15),
+          CustomSearchFiled(
+            label: 'Search in Market App',
+            searchFunction: () {},
+          ),
+          SizedBox(height: 15),
+          ProductList(),
+        ],
+      ),
     );
   }
 }
