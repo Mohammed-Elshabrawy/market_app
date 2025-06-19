bbe3f03a707da3679bb03a17e11f6142a19a6158
Mohammed-Elshabrawy <m.e18520029@gmail.com>
2025-06-19 03:28:00 +0300

    finish FavScreen.dart

--- lib/modules/favorite/ui/fav_screen.dart
+++ lib/modules/favorite/ui/fav_screen.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
+import '../../../shared/components/product_list.dart';
+
 class FavScreen extends StatelessWidget {
   const FavScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Hello Fav'),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView(
+        children: [
+          Center(
+            child: Text(
+              "Your Favorite products",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                height: 1,
+              ),
+            ),
+          ),
+          SizedBox(height: 15),
+          ProductList(),
+        ],
+      ),
     );
   }
 }
