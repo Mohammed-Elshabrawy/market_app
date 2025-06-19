fb09068de79ea03116369c8dda465210a068eb98
Mohammed-Elshabrawy <m.e18520029@gmail.com>
2025-06-19 08:53:49 +0300

    supaBase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:market_app/shared/network/remoote/supabase_key.dart';
 import 'package:market_app/shared/styles/style.dart';
-
 import 'modules/navbar/ui/main_home_screen.dart';
 
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main()  async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: url,
+    anonKey: anonKey,
+  );
   runApp(const Market());
 }
 
