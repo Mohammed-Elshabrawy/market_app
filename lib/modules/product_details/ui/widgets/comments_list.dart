8e280d16de05bf3dc9d12decda3788aef610a8e5
Mohammed-Elshabrawy <m.e18520029@gmail.com>
2025-06-24 09:29:49 +0300

    add comments time and date

--- lib/modules/product_details/ui/widgets/comments_list.dart
+++ lib/modules/product_details/ui/widgets/comments_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../../../models/product_model.dart';
@@ -15,21 +16,22 @@ class CommentsList extends StatelessWidget {
           .eq('for_product', product.productId!)
           .order('created_at', ascending: false),
       builder: (context, asyncSnapshot) {
-        List<Map<String, dynamic>> data = asyncSnapshot.data!;
+        SupabaseStreamEvent? data = asyncSnapshot.data;
         if (asyncSnapshot.connectionState == ConnectionState.waiting) {
           return Center(child: CircularProgressIndicator());
         } else if (asyncSnapshot.hasData) {
           return ListView.separated(
             shrinkWrap: true,
             physics: NeverScrollableScrollPhysics(),
-            itemBuilder: (context, index) => UserComment(),
+            itemBuilder: (context, index) =>
+                UserComment(commentsData: data[index]),
             separatorBuilder: (context, index) => Divider(),
-            itemCount: 10,
+            itemCount: data!.length,
           );
-        } else if (asyncSnapshot.hasError) {
-          return Center(child: Text('Error: ${asyncSnapshot.error}'));
+        } else if (!asyncSnapshot.hasData) {
+          return Center(child: Text('No Comments Yet'));
         } else {
-          return Center(child: Text('No data available'));
+          return Center(child: Text('Error: ${asyncSnapshot.error}'));
         }
       },
     );
@@ -37,33 +39,52 @@ class CommentsList extends StatelessWidget {
 }
 
 class UserComment extends StatelessWidget {
-  const UserComment({super.key});
+  UserComment({super.key, required this.commentsData});
+  final Map<String, dynamic>? commentsData;
 
   @override
   Widget build(BuildContext context) {
+    var date = DateTime.parse(commentsData!['created_at']);
+    final dateFormat = DateFormat.yMEd().format(date);
+    final timeFormat = DateFormat.Hm().format(date);
+
     return Column(
       children: [
         Row(
           children: [
             Text(
-              "user Name",
+              commentsData!['user_name'] ?? "user Name",
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-          ],
-        ),
-        SizedBox(height: 10),
-        Row(children: [Text("comment 1")]),
-        SizedBox(height: 10),
-        Row(
-          children: [
+            Spacer(),
             Text(
-              "Replay",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              " $dateFormat at $timeFormat",
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
             ),
           ],
         ),
         SizedBox(height: 10),
-        Row(children: [Text("Replay 1")]),
+        Row(children: [Text(commentsData!['comment'] ?? "comment")]),
+        SizedBox(height: 10),
+        commentsData!['replay'] != null
+            ? Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'replay:-',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(children: [commentsData!['replay']]),
+                ],
+              )
+            : Container(),
         SizedBox(height: 10),
       ],
     );
