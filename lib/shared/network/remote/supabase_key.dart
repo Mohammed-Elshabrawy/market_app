f635d1395eebfafb402e65f12b9cbd2ac26e4418
Mohammed-Elshabrawy <m.e18520029@gmail.com>
2025-06-26 11:46:46 +0300

    responsive

---
+++ lib/shared/network/remote/supabase_key.dart
@@ -0,0 +1,3 @@
+String url ="https://vtfhpdkerwprjahtlmrj.supabase.co";
+String anonKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ0ZmhwZGtlcndwcmphaHRsbXJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzMDg1MTYsImV4cCI6MjA2NTg4NDUxNn0.kZn1C-HO6Ld50rT6yO_wX2YnKeEaWjNvAlndW4HDlGw";
+String baseUrl ='https://vtfhpdkerwprjahtlmrj.supabase.co/rest/v1/';
